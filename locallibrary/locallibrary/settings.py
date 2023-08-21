e31f2ca5a4a28644df2c5bcc623f11ca8b979883
rzngnam1402 <legiangnam1402@gmail.com>
2023-08-21 13:06:41 +0700

    Django Tutorial Part 2: Creating a skeleton website

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add our new application
+    'catalog.apps.CatalogConfig'
+    # This object was created for us in /catalog/apps.py
 ]
 
 MIDDLEWARE = [
